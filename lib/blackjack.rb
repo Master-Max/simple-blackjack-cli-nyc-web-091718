03b6e116889aa02292182c729cd3459a2859066d
Max Petersen <mglxp5@gmail.com>
2018-09-02 20:05:21 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -15,10 +15,13 @@ end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  userMove = gets.chomp
+  return userMove
 end
 
 def end_game
